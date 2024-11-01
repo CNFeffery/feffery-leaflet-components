1fc46071803960232141c615f96ba35f87f207c7
CNFeffery <fefferypzy@gmail.com>
2024-11-01 10:12:08 +0800

    chore: 组件重分类 #6

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,66 +1,80 @@
 /* eslint-disable import/prefer-default-export */
+// 基础组件
 import LeafletMap from './components/LeafletMap.react';
+// 底图
 import LeafletTileLayer from './components/LeafletTileLayer.react';
-import LeafletGeoJSON from './components/LeafletGeoJSON.react';
-import LeafletHeatMap from './components/LeafletHeatMap.react';
-import LeafletMapAction from './components/LeafletMapAction.react';
-import LeafletMapListener from './components/LeafletMapListener.react';
-import LeafletFlowLayer from './components/LeafletFlowLayer.react';
-import LeafletCircle from './components/LeafletCircle.react';
+// 矢量
 import LeafletCircleMarker from './components/LeafletCircleMarker.react';
+import LeafletMarker from './components/LeafletMarker.react';
 import LeafletPolyline from './components/LeafletPolyline.react';
-import LeafletPolygon from './components/LeafletPolygon.react';
 import LeafletRectangle from './components/LeafletRectangle.react';
+import LeafletCircle from './components/LeafletCircle.react';
+import LeafletPolygon from './components/LeafletPolygon.react';
+import LeafletGeoJSON from './components/LeafletGeoJSON.react';
+import LeafletVectorTile from './components/LeafletVectorTile.react';
+// 附加内容
 import LeafletTooltip from './components/LeafletTooltip.react';
 import LeafletPopup from './components/LeafletPopup.react';
-import LeafletMiniMap from './components/LeafletMiniMap.react';
-import LeafletAntPath from './components/LeafletAntPath.react';
-import LeafletLayerGroup from './components/LeafletLayerGroup.react';
-import LeafletMarker from './components/LeafletMarker.react';
-import LeafletSuperCluster from './components/LeafletSuperCluster.react';
-import LeafletTileSelect from './components/LeafletTileSelect.react';
-import LeafletFullscreenControl from './components/LeafletFullscreenControl.react';
-import LeafletStaticHeatMap from './components/LeafletStaticHeatMap.react';
-import LeafletExport from './components/LeafletExport.react';
+// 容器
 import LeafletFeatureGroup from './components/LeafletFeatureGroup.react';
-import LeafletVectorTile from './components/LeafletVectorTile.react';
-import LeafletDomWrapper from './components/LeafletDomWrapper.react';
+import LeafletLayerGroup from './components/LeafletLayerGroup.react';
+// 特殊图层
+import LeafletAntPath from './components/LeafletAntPath.react';
+import LeafletFlowLayer from './components/LeafletFlowLayer.react';
+import LeafletHeatMap from './components/LeafletHeatMap.react';
+import LeafletImageOverlay from './components/LeafletImageOverlay.react';
+import LeafletStaticHeatMap from './components/LeafletStaticHeatMap.react';
+import LeafletSuperCluster from './components/LeafletSuperCluster.react';
 import EsriTiledMapLayer from './components/esri/EsriTiledMapLayer.react';
-import Fragment from './components/Fragment.react';
+// 特殊功能
+import LeafletExport from './components/LeafletExport.react';
+import LeafletFullscreenControl from './components/LeafletFullscreenControl.react';
+import LeafletMapAction from './components/LeafletMapAction.react';
+import LeafletMapListener from './components/LeafletMapListener.react';
+import LeafletMiniMap from './components/LeafletMiniMap.react';
+import LeafletTileSelect from './components/LeafletTileSelect.react';
+import LeafletDomWrapper from './components/LeafletDomWrapper.react';
 import LeafletMapProvider from './components/LeafletMapProvider.react';
 import LeafletMapSync from './components/LeafletMapSync.react';
-import LeafletImageOverlay from './components/LeafletImageOverlay.react';
+import Fragment from './components/Fragment.react';
 
 export {
+    // 基础组件
     LeafletMap,
+    // 底图
     LeafletTileLayer,
-    LeafletGeoJSON,
-    LeafletHeatMap,
-    LeafletMapAction,
-    LeafletMapListener,
-    LeafletFlowLayer,
-    LeafletCircle,
+    // 矢量
     LeafletCircleMarker,
+    LeafletMarker,
     LeafletPolyline,
-    LeafletPolygon,
     LeafletRectangle,
+    LeafletCircle,
+    LeafletPolygon,
+    LeafletGeoJSON,
+    LeafletVectorTile,
+    // 附加内容
     LeafletTooltip,
     LeafletPopup,
-    LeafletMiniMap,
-    LeafletAntPath,
-    LeafletLayerGroup,
-    LeafletMarker,
-    LeafletSuperCluster,
-    LeafletTileSelect,
-    LeafletFullscreenControl,
-    LeafletStaticHeatMap,
-    LeafletExport,
+    // 容器
     LeafletFeatureGroup,
-    LeafletVectorTile,
-    LeafletDomWrapper,
+    LeafletLayerGroup,
+    // 特殊图层
+    LeafletAntPath,
+    LeafletFlowLayer,
+    LeafletHeatMap,
+    LeafletImageOverlay,
+    LeafletStaticHeatMap,
+    LeafletSuperCluster,
     EsriTiledMapLayer,
-    Fragment,
+    // 特殊功能
+    LeafletExport,
+    LeafletFullscreenControl,
+    LeafletMapAction,
+    LeafletMapListener,
+    LeafletMiniMap,
+    LeafletTileSelect,
+    LeafletDomWrapper,
     LeafletMapProvider,
     LeafletMapSync,
-    LeafletImageOverlay
+    Fragment
 };
